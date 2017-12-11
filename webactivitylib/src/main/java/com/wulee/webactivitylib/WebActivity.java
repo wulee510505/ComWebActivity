f42c9b69f7302ce585b6814b67a13dd6eb7944b3
wulee510505 <wulee510505@gmail.com>
2017-12-11 14:16:10 +0800

    删除分享按钮

--- webactivitylib/src/main/java/com/wulee/webactivitylib/WebActivity.java
+++ webactivitylib/src/main/java/com/wulee/webactivitylib/WebActivity.java
@@ -16,7 +16,6 @@ public class WebActivity extends AppCompatActivity implements WebFragment.OnWebV
 
     private RelativeLayout titleLayout;
     private ImageView ivBack;
-    private TextView tvRight;
     private TextView tvTitle;
 
     private WebFragment mWebFragment;
@@ -36,7 +35,7 @@ public class WebActivity extends AppCompatActivity implements WebFragment.OnWebV
         intent.putExtra("title", title);
         intent.putExtra("url", url);
         intent.putExtra("bgTitleColorRes", bgTitleColorRes);
-        from.startActivityForResult(intent,0);
+        from.startActivity(intent);
     }
 
     @Override
@@ -51,12 +50,10 @@ public class WebActivity extends AppCompatActivity implements WebFragment.OnWebV
     private void initView() {
         titleLayout =  (RelativeLayout) findViewById(R.id.titlelayout);
         ivBack =  (ImageView) findViewById(R.id.iv_back);
-        tvRight =  (TextView) findViewById(R.id.tv_share);
         tvTitle =  (TextView) findViewById(R.id.title);
         mProgressBar = (ProgressBar) findViewById(R.id.pb_web);
 
         ivBack.setOnClickListener(this);
-        tvRight.setOnClickListener(this);
     }
 
 
@@ -106,8 +103,6 @@ public class WebActivity extends AppCompatActivity implements WebFragment.OnWebV
         int id = view.getId();
         if (id == R.id.iv_back) {
             finish();
-        }else  if (id == R.id.tv_share) {
-            Utils.shareTextAndImage(this,title,url, null);
         }
     }
 
